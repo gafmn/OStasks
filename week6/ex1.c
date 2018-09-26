efe123f4dbef064b152f4e3d5585612d57ebcd21
Madina Gafarova <34398145+gafmn@users.noreply.github.com>
2018-09-26 16:32:55 +0300

    Update ex1.c

--- week6/ex1.c
+++ week6/ex1.c
@@ -1,26 +1,27 @@
+//
+// Created by Madina Gafarova on 26/09/2018.
+//
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h> 
-#include<sys/types.h> 
-#include<string.h> 
+#include<unistd.h>
+#include<sys/types.h>
+#include<string.h>
 #include<sys/wait.h>
 
 int main(){
-	int readWrite[2];
+    int readWrite[2];
 
-	char str[] = "OS";
-	char res[] = "";
-	pid_t pipe;
+    char str[] = "OS";
+    char res[] = "";
 
-	if (pipe(readWrite) == -1){
-		printf("%s","Pipe failed");
-		return 1;
-	} 
+    if(pipe(readWrite) == -1){
+        return -1;
+    }
 
-	write(readWrite[1], str, strlen(str) + 1);
-	close(readWrite[1]);
-	read(readWrite[0], res, strlen(str) + 1);
-	close(readWrite[0]);
-
-	return 0;
+    write(readWrite[1], str, strlen(str) + 1);
+    close(readWrite[1]);
+    read(readWrite[0], res, strlen(str) + 1);
+    close(readWrite[0]);
+    printf("Second pipe %s\n", res);
+    return 0;
 }
