d0897781873e37f0f7d226cdaaa0481f99d59a3e
Madina Gafarova <34398145+gafmn@users.noreply.github.com>
2018-11-01 23:27:24 +0300

    Update ex2.c

--- week11/ex2.c
+++ week11/ex2.c
@@ -5,17 +5,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(){
-    printf("H");
-    sleep(1);
-    printf("e");
-    sleep(1);
-    printf("l");
-    sleep(1);
-    printf("l");
-    sleep(1);
-    printf("o");
-    sleep(1);
-    
+int main() {
+    setvbuf(stdout, NULL, _IOLBF, 0);
+    char buffer[5] = "Hello";
+    for (int i = 0; i < 5; i++) {
+        printf("%c", buffer[i]);
+        sleep(1);
+    }
     return 0;
 }
