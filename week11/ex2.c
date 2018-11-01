1fcf52d625bb73c824d473a09bc6393235684f98
Madina Gafarova <34398145+gafmn@users.noreply.github.com>
2018-11-01 17:55:33 +0300

    Add files via upload

---
+++ week11/ex2.c
@@ -0,0 +1,21 @@
+//
+// Created by Madina Gafarova on 01/11/2018.
+//
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+    printf("H");
+    sleep(1);
+    printf("e");
+    sleep(1);
+    printf("l");
+    sleep(1);
+    printf("l");
+    sleep(1);
+    printf("o");
+    sleep(1);
+    
+    return 0;
+}
